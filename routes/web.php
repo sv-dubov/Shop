89f9f659b55c910fa758d9575b7a192836704fdc
Sviatoslav <sv_dubov@ukr.net>
2021-05-24 20:29:11 +0300

    Made Index controller and view

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\IndexController')->name('index');
 
 Route::get('/catalog/index', 'App\Http\Controllers\CatalogController@index')->name('catalog.index');
 Route::get('/catalog/category/{slug}', 'App\Http\Controllers\CatalogController@category')->name('catalog.category');
